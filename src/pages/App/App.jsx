1598f7cae70330abbb4c18212127469fa97a57ff
Ryan Gonzales <helloryantg@gmail.com>
2018-12-17 19:28:42 -0800

    add login and signup components

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,13 +4,49 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import userService from '../../utils/userService';
+
 import './App.css';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
 
 class App extends Component {
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({user: null});
+  }
+
+  componentDidMount() {
+    let user = userService.getUser();
+    this.setState({user});
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <Router>
+          <Switch>
+            <Route exact path='/' render={() =>
+              <div>Hi</div> 
+            } />
+            <Route exact path='/signup' render={({history}) => 
+              <SignupPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+              } />
+              <Route exact path='/login' render={(props) =>
+              <LoginPage 
+                {...props}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            }/>
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './LoginPage.css';
+import userService from '../../utils/userService';
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      pw: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+        [e.target.name] : e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    userService.login(this.state)
+      .then(() => {
+        this.props.handleSignupOrLogin();
+        this.props.history.push('/');
+      })
+      .catch(err => alert('Invalid Credentials'));
+  }
+
+  render() {
+    return (
+      <div className="LoginPage">
+        <form onSubmit={this.handleSubmit} >
+          <div>
+            <div>
+              <input type="email" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange} />
+            </div>
+          </div>
+          <div>
+            <div>
+              <input type="password" placeholder="Password" value={this.state.pw} name="pw" onChange={this.handleChange} />
+            </div>
+          </div>
+          <div>
+            <div>
+              <button>Log In</button>&nbsp;&nbsp;&nbsp;
+              <Link to='/'>Cancel</Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+};
+
+export default LoginPage;
